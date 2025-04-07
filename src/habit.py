6b58adcec6cae7d27f6a69f28233f6737b12b20f
RaghadIU <raghadabashar293@gmail.com>
2025-04-07 20:41:01 +0300

    Update habit.py

--- src/habit.py
+++ src/habit.py
@@ -1,25 +1,27 @@
 from datetime import datetime
 from typing import List
 
+
 class Habit:
-    def __init__(self, name: str, description: str, frequency: str, created_at: str = None):
+    def __init__(self, id: int, name: str, description: str, frequency: str, created_at: str, streak: int = 0, completion_dates: List[str] = None):
         """
-       Class represents the habit that a user can add and track.
+        Represents a habit that the user wants to track.
+
         :param id: Unique identifier for the habit
-        :param name: Habit Name
+        :param name: Name of the habit
+        :param frequency: Frequency of the habit (daily or weekly)
         :param description: Optional description of the habit
-        :param frequency: frequent the habit ('daily' or 'weekly')
-        :param created_at: Custom creation date (by default is current time)
+        :param created_at: Creation date of the habit (default: current time)
         :param streak: Consecutive success count
         :param completion_dates: List of dates when habit was marked completed
         """
         self.id = id
         self.name = name
-        self.description = description
         self.frequency = frequency
-        self.created_at = created_at if created_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.description = description
+        self.created_at =created_at if created_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.streak = streak
-        self.completion_dates = completion_dates if completion_dates else []
+        self.completion_dates = completion_dates if completion_dates else[]
 
     def complete_habit(self):
         """
@@ -38,7 +40,6 @@ class Habit:
             if weekday not in self.completion_dates:
                 self.completion_dates.append(weekday)
                 self.streak += 1
-
-
+                
     def __repr__(self):
-        return f"Habit(id={self.id}, name={self.name}, description={self.description}, frequency={self.frequency}, created_at={self.created_at}, streak={self.streak}, complete_habit={self.complete_habit})"
+        return f"Habit(id={self.id}, name={self.name}, frequency={self.frequency}, description={self.description}, created_at={self.created_at}, streak={self.streak}, complete_habit={self.complete_habit})"
